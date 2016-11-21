9374f37aca92ca60a60ea2fd6c3083176fca33f8
Cosas1008 <yuweiichen@gmail.com>
2016-11-21 09:19:42 +0800

    First commit on Nov 21st

---
+++ src/RobotGetPosition.java
@@ -0,0 +1,11 @@
+
+public class RobotGetPosition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final int maxBuffer = 1024;
+		byte[] command = new byte[maxBuffer];
+		SendUDP(command)
+	}
+
+}
