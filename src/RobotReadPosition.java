9eb92c04e716a1fa93d34c8f72f1773bc9bb25ec
Yuwei <yuweiichen@gmail.com>
2016-11-30 00:47:46 +0800

    November 300th, 2016

--- src/RobotReadPosition.java
+++ src/RobotReadPosition.java
@@ -10,12 +10,19 @@
 public class RobotReadPosition extends SendUDP {
     private static int[] readInt = { 89, 69, 82, 67, 32, 00, 00, 00, 03, 01, 00, 00, 00, 00, 00, 00, 57, 57, 57, 57, 57,
 	    57, 57, 57, 117, 00, 101, 00, 00, 01, 00, 00 };
-
+	    private int[] response = new int[]{};
+	    private int[] tool = new int[8];
     public RobotReadPosition() throws Exception {
-	super(readInt);
-	RobotReadPosition r = new RobotReadPosition();
-	int[] response = new int[]{};
-	response = r.sendint();
+		super(readInt);
+		RobotReadPosition r = new RobotReadPosition();
+		this.response = r.sendint();
+    }
+
+    private static void main(String[] args){
+    	//The response store data form we have to modified to tool
+    	for(int i :response){
+
+    	}
     }
 
 }
